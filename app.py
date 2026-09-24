2a28fe34d4bcbfcde61c6293ac7472c81def2f63
logesh40466 <logu40466@gmail.com>
2026-09-24 15:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predict():
     img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
 
     # Run inference
-    results = model.predict(source=img, conf=0.25, imgsz=320)
+    results = model.predict(source=img, conf=0.15, imgsz=640)
     res = results[0]
 
     # Draw bounding boxes
